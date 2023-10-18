63e8042a835e0c1ed38d721a7398e7d6389e4dca
David Carmichael <carmichael.db88@gmail.com>
2023-10-18 23:25:20 +0000

    complete wordsearch function and tests

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,7 +4,7 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return false if the word is not present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +31,62 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARRY');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LOUIS');
+
+    assert.isFalse(result);
+
+    
+  });
+
+  it('should return false if the array is empty', function() {
+    const result = wordSearch([], 'LARRY');
+    assert.isFalse(result);
+  });
+
+  it('should return "No word Provided" if no string is given', function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 5);
+    assert.strictEqual(result, "No word Provided");
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,48 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+/**
+ * Function inputs 2d array containing letters, and a string of a word
+ * searches arrays for word in the 2d array
+ * returns true if word exists horizontally or vertically
+ * if array is empty returns false
+ * if word is not a string returns error "No word Provided"
+ * Pair Programming: Lous Stafford, David Carmichael
+ */
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  const arr = matrix; // copy input to different variable
+  matrix = []; // reassign matrix as empty array to create return so return isn't modified from provided code.
+  if (Array.isArray(arr) && Array.isArray(arr[0])) { // check if input is array, and first index contains array
+    for (let x of arr[0]) { // creates new arrays with each index of first array for new first column
+      matrix.push([x]);
+    }
+    for (let i = 1; i < arr.length; i++) { // iterates over remaining input's elements
+      if (Array.isArray(arr[i])) { // verifies each element is an array
+        for (let z = 0; z < matrix.length; z++) { // iterates over the array
+          matrix[z].push(arr[i][z]); //pushes value at each index to the array within the matrix at the appropriate row.
+        }
+      }
+    }
+  }
+  return matrix;
+};
+
+const wordSearch = (letters, word) => {
+  if (typeof(word) !== 'string') { // if word is not a string returns error
+    const error = "No word Provided";
+    return error;
+  }
+  const horizontalJoin = letters.map(ls => ls.join('')); //searches 2d array of letters horizontally
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  const verticalJoin = transpose(letters).map(ls => ls.join('')); //searches 2d array of letters vertically and uses transpose function
+  for (i of verticalJoin) {
+    if (i.includes(word)) {
+      return true;
+    }
+  }
+  return false; //returns false if word is not found in 2d array
+};
+
+module.exports = wordSearch;
